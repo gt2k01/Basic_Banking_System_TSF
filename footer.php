cfdfbe8b6042b28dc283d28c0d824c6f02eb97f5
gt2k01 <56781836+gt2k01@users.noreply.github.com>
2022-07-17 23:25:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">Made by Tushaar Gusain <br>
+      For GRIPJULY22 @The Sparks Foundation</div>
+ </div>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include('nav.php') ?>
+
+
+  <h1>WELCOME TO TSF BANK</h1>
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="view_users.php"><button class="btn" id="red">View All Users</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Transfer Funds</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">Transaction History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
